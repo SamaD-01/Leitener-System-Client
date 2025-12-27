d1d4fab0f2bebdf9c1278bed12aea99d5112d808
SamaD-01 <abdessamad.ajja00@gmail.com>
2025-12-27 23:39:17 +0100

    bugfix:tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/test/setup.ts",
+    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["**/node_modules/**", "**/dist/**", "**/src/test/e2e/**"]
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
